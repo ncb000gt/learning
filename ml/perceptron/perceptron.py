bfaeb46500d6cc44929f0ba1a7b6fa77e1eecebd
Nick Campbell <nicholas.j.campbell@gmail.com>
2013-02-07 09:47:31 -0500

    Updated perceptron to operate on vectors and do no input munging.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- ml/perceptron/perceptron.py
+++ ml/perceptron/perceptron.py
@@ -6,19 +6,17 @@ import string, random
 parser = argparse.ArgumentParser(description="Prepare for perceptron.")
 parser.add_argument("--input",required=True,help="Specify the input data file.")
 parser.add_argument("--iterations",help="Number of iterations.",type=int,default=5)
-parser.add_argument("--train",help="What file are we training the model from.")
-parser.add_argument("--test",help="What file are we testing the trained model from.")
+parser.add_argument("--train",action="store_true")
+parser.add_argument("--test",action="store_true")
 parser.add_argument("--model",help="Where are we dumping the model after generated.")
 args = parser.parse_args()
 
-documents = {}
 inputs = []
-training = {}
 
 BIAS = -1
 THRESHOLD = .75 # > = spam
 LEARNING_RATE = .2
-NEURONS = 2 
+NEURONS = 1
 INPUTS = 0
 
 def load_model():
@@ -29,51 +27,27 @@ def load_model():
 
 def initialize_data():
 	data = pickle.load(open(args.input, "r"))
-	documents = data["documents"]
-	inputs = sorted(data["words"].keys())
-	return documents, inputs, len(inputs) + 1
+	vectors = data["vectors"]
+	inputs = data["inputs"]
+	return vectors, inputs, len(inputs)
 
 def initialize_random_weights():
 	weights = []
 	for i in range(INPUTS):
 		weight = []
 		for j in range(NEURONS):
+			pn = 1
 			if random.random() > .5:
-				pn = 1
-			else:
 				pn = -1
 			weight.append(pn * random.random())
 		weights.append(weight)
 	return weights
 
-def initialize_comparison_data(f):
-	data = {}
-	for line in f.readlines():
-		split = line.split()
-		t = []
-		if split[0] == "spam":
-			t = [1, 0]
-		else:
-			t = [0, 1]
-		f = split[1].split("/")[-1]
-		data[f] = t
-	return data
-
-def assign_input_values(words):
-	tmp = [BIAS]
-	for i in range(INPUTS-1):
-		_input = inputs[i]
-		if _input in words:
-			tmp.append(words[_input])
-		else:
-			tmp.append(0)
-	return tmp
-
-def g(doc_inputs):
-	neurons = [0 for j in range(NEURONS)] #initialize activations
+def g(weights, vector):
+	neurons = [-1.0 for j in range(NEURONS)] #initialize activations
 	for j in range(NEURONS):
 		for i in range(INPUTS):
-			neurons[j] += (doc_inputs[i] * weights[i][j])
+			neurons[j] = neurons[j] + vector[i] * weights[i][j]
 	return neurons
 
 def activation(neurons):
@@ -85,30 +59,30 @@ def activation(neurons):
 			activated.append(0)
 	return activated
 
-def adjust_weights(doc_inputs, y, t):
+def adjust_weights(weights, vector, y, t):
 	for j in range(NEURONS):
 		for i in range(INPUTS):
-			weights[i][j] = weights[i][j] + (LEARNING_RATE * (t[j] - y[j]) * doc_inputs[i])
+			weights[i][j] = weights[i][j] + (LEARNING_RATE * (t[j] - y[j]) * vector[i])
+	return weights
 
-def run():
+def run(vectors, weights):
 	decisions = {}
-	for document in documents:
-		doc = documents[document]
-		doc_inputs = assign_input_values(doc["words"])
-		neurons = g(doc_inputs)
+	for f in vectors:
+		doc = vectors[f]
+		vector = doc["vector"][0]
+		neurons = g(weights, vector)
+		#print f, neurons
 		activated_neurons = activation(neurons)
-		decisions[document] = activated_neurons
-		#print document, training[document], activated_neurons
+		decisions[f] = activated_neurons
 		if args.train:
-			adjust_weights(doc_inputs, activated_neurons, training[document])
-	return decisions
+			weights = adjust_weights(weights, vector, activated_neurons, doc["vector"][-1])
+	return decisions, weights
 
-def test_neurons(results):
-	test_data = initialize_comparison_data(open(args.test, "r"))
+def test_neurons(results, vectors):
 	print results
 	for result in results:
 		_result = results[result]
-		_test = test_data[result]
+		_test = vectors[result]["vector"][-1]
 		for j in range(NEURONS):
 			if _result[j] <> _test[j]:
 				print result, "INCORRECT MATCH:", _result, _test
@@ -122,28 +96,24 @@ def spam_or_ham(results):
 def save_model(model):
 	pickle.dump(model, open(args.model, "w"))
 
-
-
-
 #stop using functions and actually do something!
-documents, inputs, INPUTS = initialize_data()
+vectors, inputs, INPUTS = initialize_data()
 if args.model and not args.train:
 	weights, inputs, INPUTS = load_model()
 else:
 	weights = initialize_random_weights()
-if args.train:
-	training = initialize_comparison_data(open(args.train, "r"))
+
 
 results = None
 if args.train:
 	for i in range(args.iterations):
 		start = time.time()
-		print "Started iteration:",i
-		run()
+		#print "Started iteration:",i
+		results, weights = run(vectors, weights)
 		end = time.time()
-		print "Completed iteration:",i,"and took",(end - start),"s."
+		#print "Completed iteration:",i,"and took",(end - start),"s."
 else:
-	results = run()
+	results, weights = run(vectors, weights)
 
 model = {
 		"weights": weights,
@@ -154,6 +124,6 @@ if args.train and args.model:
 elif args.train:
 	print model
 elif args.test:
-	test_neurons(results)
+	test_neurons(results, vectors)
 else:
 	spam_or_ham(results)
